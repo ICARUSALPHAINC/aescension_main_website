fda082d827073af0f8577f2e081ba16830318fa3
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2025-03-05 16:09:03 -0800

    fixed incorrect path and added game info section

--- main-page/src/App.js
+++ main-page/src/App.js
@@ -16,7 +16,8 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import navBar from "./components/navBar";
 import ResponsiveAppbar from "./components/navBar";
-import Homepage from "./components/HomePage";
+import Homepage from "./components/homePage";
+import GameInfo from "./components/gameInfo";
 
 const pages = ["Game Info", "Heroes", "Community"];
 const settings = ["Log In", "Profile", "Settings", "Sign Up"];
@@ -26,6 +27,7 @@ function App() {
     <>
       <ResponsiveAppbar />
       <Homepage/>
+      <GameInfo/>
     </>
   );
 }
